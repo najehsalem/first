2f6d6eb9da2a0d4fb04778392ccfe3d5a74cddb4
najehsalem <najehsalem2023@gmail.com>
2023-11-26 20:20:13 +0100

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'appareils';
+ {name:'machine alaver', statut:'Eteindre' },
+  {name:'Tv', statut:'Allumer' },
+  {name:'pc', statut:'Eteindre' },
+  {name:'Climatiseur', statut:'Allumer' }
+
+]
+constructor(){
+  setTimeout(()=>{
+    this.isAuth=!this.isAuth;
+  },4000);
+  
+}
+onAllumerTout(){
+  console.log("Bouton Allumer tout clicked!!!")
+}
+
+eteindreTout(){
+  console.log("Bouton Eteindre tout tout clicked!!!")
+}
+
+}
+
